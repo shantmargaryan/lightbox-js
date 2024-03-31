bb08de2256fef98a704e6700ac2cab280ee6b95d
Shant <shant212205@gmail.com>
2024-03-31 06:20:20 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+const galleryItem = document.querySelectorAll(".gallery__item");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox__img");
+const lightboxClose = document.querySelector(".lightbox__close");
+const lightboxNext = document.querySelector(".lightbox__next");
+const lightboxPrev = document.querySelector(".lightbox__prev");
+
+
+let index = 1
+
+
+function showLightBox(lightBox) {
+  if (lightbox > galleryItem.length) {
+    index = 1
+  } else if (lightbox < 1) {
+    index = galleryItem.length
+  }
+
+  let imgLocation = galleryItem[index - 1].children[0].getAttribute("src");
+  lightboxImg.setAttribute("src", imgLocation);
+}
+
+function currentImg() {
+  lightbox.style.display = "flex";
+
+  let imageIndex = parseInt(this.getAttribute("data-item"));
+  showLightBox(index = imageIndex);
+}
+
+for (let i = 0; i < galleryItem.length; i++) {
+  galleryItem[i].addEventListener("click", currentImg);
+}
+
+function sliderImage(slider) {
+  showLightBox(index += slider);
+}
+
+function prevImage() {
+  if (index === 1) {
+    index = galleryItem.length
+  }
+  showLightBox(index -= 1);
+}
+
+function nextImage() {
+  if (index === galleryItem.length) {
+    index = 1
+  }
+  showLightBox(index += 1);
+}
+
+lightboxPrev.addEventListener("click", prevImage);
+lightboxNext.addEventListener("click", nextImage);
+
+
+lightbox.addEventListener("click", (event) => {
+  if (event.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+  if (event.target === lightboxClose) {
+    lightbox.style.display = "none";
+    
+  }
+})
